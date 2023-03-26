46d7911df384f5e3ae0df2439d1113f813a77d69
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-26 14:36:05 +0900

    feat: 세팅완료

---
+++ src/test/java/querydsl/performanceimprovement/PerformanceImprovementApplicationTests.java
@@ -0,0 +1,13 @@
+package querydsl.performanceimprovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceImprovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
