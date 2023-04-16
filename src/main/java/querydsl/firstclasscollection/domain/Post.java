d10ad952b82febd3f395a2c3c8b21af285a28614
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-16 13:26:45 +0900

    feat: entity 구현

---
+++ src/main/java/querydsl/firstclasscollection/domain/Post.java
@@ -0,0 +1,18 @@
+package querydsl.firstclasscollection.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String title;
+
+    @Column(length = 500, nullable = false)
+    private String content;
+}
---
+++ src/main/java/querydsl/firstclasscollection/domain/Tag.java
@@ -0,0 +1,19 @@
+package querydsl.firstclasscollection.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tag")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(length = 10, nullable = false)
+    private String title;
+}
